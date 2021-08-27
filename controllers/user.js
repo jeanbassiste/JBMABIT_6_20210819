873624daa46c1a0eed94602fcd56cf603fdf00c5
jeanbassiste <mabit.jeanbaptiste@gmail.com>
2021-08-27 09:20:51 +0200

    TOKEN fonctionne

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,6 @@
 const User = require('../models/Users');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const env = require('dotenv');
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
@@ -19,17 +18,25 @@ exports.signup = (req, res, next) => {
         ).catch(
             (error) => {
                 //find sur la propriété des objets ? Trouver comment afficher l'erreur, quelle qu'elle soit
-                //console.log(Object.getOwnPropertyNames(error.errors));
-                console.log(error.errors.email);
+                //console.log('L erreur est : ' + Object.getOwnPropertyNames(error.errors.email.properties.message));
+                //console.log(error.errors.email);
                 console.log(Object.getOwnPropertyNames(error.errors));
-                var message = "" ;
+                var message = [];
+                //console.log(Object.getOwnPropertyNames(error.errors.email.properties.message));
+
+                console.log(error.errors.email.properties.message);
+                //console.log(error.errors[email.properties.message]);
                 Object.getOwnPropertyNames(error.errors).forEach(function (element) {
                     console.log(element);
-                    //message += value;
+                    //console.log(error.errors[element.properties.message]);
+                    //var errType = element.properties.message;
+                    var err = error.errors.email.properties.message;
+                    //console.log(errType);
+                    message += err;
                 });
                 //console.log(message);
                 res.status(400).json({
-                    //message: error.errors.email.properties.message
+                    //message: error.errors.email.properties.message,
                     message: message
                 });
             }
@@ -39,26 +46,31 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
+    console.log(process.env.TOKEN);
     User.findOne({ email: req.body.email })
     .then(user => {
         if (!user) {
-            return res.status(401).json({ error: 'Utilisateur inexistant'});
+            //console.log('did not work 0');
+            return res.status(404).json({ message: 'Utilisateur inexistant'});            
         }
         bcrypt.compare(req.body.password, user.password)
         .then(valid => {
             if(!valid) {
-                return res.status(401).json({ error : 'Mot de passe incorect'});
+                //console.log('did not work 1');
+                return res.status(400).json({ message: 'Mot de passe incorect'});
+                
             }
             res.status(200).json({
                 userId: user._id,
                 token: jwt.sign(
                     { userId: user._id },
-                    'RANDOM_TOKEN_SECRET', //faire un process.env('token') + dotenv
+                    process.env.TOKEN,
                     { expiresIn: '24h'}
                 )
             });
         })
-        .catch(error => res.status(500).json({ error }));
+        .catch(error => res.status(500).json({ message: user }));
+        console.log('did not work');
     })
     .catch(error => res.status(500).json({ error }));
 };
