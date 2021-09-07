d9e1cbc798dab5389ac480c913880792e8da6cec
jeanbassiste <mabit.jeanbaptiste@gmail.com>
2021-09-07 23:17:18 +0200

    Route sauces OK -likes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
+const path = require('path');
 
 mongoose.connect('mongodb+srv://jeanbassiste:scoopinou@cluster0.dwfbx.mongodb.net/cluster0?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -20,6 +21,7 @@ app.use((req, res, next) => {
 
 app.use(express.json())
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/auth', userRoutes);
 app.use('/api/sauces', sauceRoutes);
 
--- routes/sauce.js
+++ routes/sauce.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const multer = require('../middleware/multer-config');
+
 const sauceCtrl = require('../controllers/sauce');
 
 router.get('/', sauceCtrl.getAllSauce);
 router.get('/:id', sauceCtrl.getSauce);
 
-router.post('/', sauceCtrl.createSauce);
-router.put('/:id', sauceCtrl.updateSauce);
+router.post('/', multer, sauceCtrl.createSauce);
+router.put('/:id', multer, sauceCtrl.updateSauce);
 router.delete('/:id', sauceCtrl.deleteSauce);
 router.post('/:id/like', sauceCtrl.likeSauce);
 
-module.exports = router;
+module.exports = router;  
