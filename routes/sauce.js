fb3b2be5c94ef9288fdf1ad25673eeeaceed030c
jeanbassiste <mabit.jeanbaptiste@gmail.com>
2021-09-07 20:30:58 +0200

    routes + app + models OK

--- routes/sauce.js
+++ routes/sauce.js
@@ -3,5 +3,12 @@ const router = express.Router();
 
 const sauceCtrl = require('../controllers/sauce');
 
+router.get('/', sauceCtrl.getAllSauce);
+router.get('/:id', sauceCtrl.getSauce);
+
+router.post('/', sauceCtrl.createSauce);
+router.put('/:id', sauceCtrl.updateSauce);
+router.delete('/:id', sauceCtrl.deleteSauce);
+router.post('/:id/like', sauceCtrl.likeSauce);
 
 module.exports = router;
