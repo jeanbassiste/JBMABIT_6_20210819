4df90ac63a01d36c1c84fdc99d335cf79d4e465f
jeanbassiste <mabit.jeanbaptiste@gmail.com>
2021-09-07 23:23:48 +0200

    authentification OK

--- routes/sauce.js
+++ routes/sauce.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
 const sauceCtrl = require('../controllers/sauce');
@@ -8,9 +9,9 @@ const sauceCtrl = require('../controllers/sauce');
 router.get('/', sauceCtrl.getAllSauce);
 router.get('/:id', sauceCtrl.getSauce);
 
-router.post('/', multer, sauceCtrl.createSauce);
-router.put('/:id', multer, sauceCtrl.updateSauce);
-router.delete('/:id', sauceCtrl.deleteSauce);
-router.post('/:id/like', sauceCtrl.likeSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, sauceCtrl.updateSauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.post('/:id/like', auth, sauceCtrl.likeSauce);
 
 module.exports = router;  
