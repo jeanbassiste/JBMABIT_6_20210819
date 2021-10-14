3aeb56f9fb3daf3b230c16e1a6e52edde9591a21
jeanbassiste <71725798+jeanbassiste@users.noreply.github.com>
2021-10-14 11:51:11 +0200

    commentaire midware auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,14 @@
+//fait appel à jsonwebtoken, notre protocole de tokenisation pour l'authentification
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
+    //prend le token de validation présent dans le header, le vérifie et récupère l'ID de l'utilisateur associé au token
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, process.env.TOKEN);
     const userId = decodedToken.userId;
+    
+    //Vérifie que l'ID utilisateur récupéré est celui attendu (celui de l'utilisateur qui essaye d'accéder à la page
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
     } else {
