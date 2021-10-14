d221684b287b0e7de8d4aeb6eb8d2408b7978a22
jeanbassiste <71725798+jeanbassiste@users.noreply.github.com>
2021-10-14 11:54:29 +0200

    com midware image

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,15 +1,20 @@
+//Middleware pour uploader une image avec multer
 const multer = require('multer');
 
+//Prépare les différents formats de fichier
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png'
 };
 
+
+//Paramètre le dossier d'enregistrerment de l'image
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, 'images');
   },
+  //Paramètre le nom du fichier
   filename: (req, file, callback) => {
     const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
