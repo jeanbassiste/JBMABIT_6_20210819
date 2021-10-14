336bc94c55422c5b29be220c391eee2133156d73
jeanbassiste <71725798+jeanbassiste@users.noreply.github.com>
2021-10-14 12:11:46 +0200

    com router utilisateur

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,11 @@
+//Configure les routes des requêtes sur les utilisateurs avec express et router
 const express = require('express');
 const router = express.Router();
 
+//Fait appel au contrôleur d'utilisateur
 const userCtrl = require('../controllers/user');
 
+//Routes pour les requêtes de connexion et de création de compte
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 
